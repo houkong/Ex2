cb87564027415bc2156a21966eba8f9cbf790b30
houkong <37408284+houkong@users.noreply.github.com>
2019-10-17 10:04:34 +0200

    Version 0.1
    Defining global params

---
+++ params.py
@@ -0,0 +1,7 @@
+#Params
+l = 1.0 #length of pendulum
+g = 9.81 #gravittional acceleration
+theta0 = 0.2 #initial angle in radians
+omega0 = 0.0 #Initial angular velocity in radians
+q = 1.0 #damping parameter
+F_D = 0.2 #Driving force
